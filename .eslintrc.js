313cf585990719afe14c2352a89d8195df9ef774
RD <rdelgado@inkling.com>
2023-01-29 13:20:05 -0500

    refine: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,35 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  ignorePatterns: ["dist", "build", "node_modules", "*.js"],
+  plugins: ['@typescript-eslint', 'eslint-plugin-import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+  ],
+  ignorePatterns: ['dist', 'build', 'node_modules'],
   rules: {
     '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
     'no-console': 'warn',
+    'import/no-named-as-default': 'off',
   },
+  settings: {
+    'import/resolver': {
+      node: true,
+      typescript: {
+        project: './tsconfig.json',
+      },
+    },
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        'no-undef': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
 }
-
